8af0ddf610d6d0fe37a5b44a615a32bddb68b9e6
ecesari <ece.sarioglu@ichoosr.com>
2023-11-04 15:02:59 +0100

    added test cases for insurance cost

--- Coolblue.Products.Tests.Unit/Application/Insurance/CalculateInsuranceQueryHandlerTests.cs
+++ Coolblue.Products.Tests.Unit/Application/Insurance/CalculateInsuranceQueryHandlerTests.cs
@@ -16,22 +16,25 @@ namespace Coolblue.Products.Tests.Unit.Application.Insurance
         }
 
         [Theory]
-        [InlineData(1, 700,1000)]
-        [InlineData(1, 700,500)]
-        public async void CalculateInsuranceQuery_Handle_GivenSalesPriceBetween500And2000Euros_ShouldAddThousandEurosToInsuranceCost(int productId, float salesPrice, double expectedValue)
+        [InlineData(1, "Foo", 700,1000)]
+        [InlineData(1, "Laptops", 400,500)]
+        [InlineData(1, "Laptops", 1200,1500)]
+        [InlineData(1, "Foo", 2500,2000)]        
+        public async void CalculateInsuranceQuery_Handle_GivenSalesPrice_ShouldAddToInsuranceCost(int productId, string productTypeName, float salesPrice, double expectedValue)
         {
-            _productDataIntegrationMock.Setup(x => x.GetProductTypeByProductAsync(productId, It.IsAny<CancellationToken>())).Returns(new CoolBlue.Products.Application.ProductType.Models.ProductTypeViewModel { HasInsurance = true, Id = productId, Name = "Foo"});
+            //setup
+            _productDataIntegrationMock.Setup(x => x.GetProductTypeByProductAsync(productId, It.IsAny<CancellationToken>())).Returns(new CoolBlue.Products.Application.ProductType.Models.ProductTypeViewModel { HasInsurance = true, Id = productId, Name = productTypeName });
             _productDataIntegrationMock.Setup(x => x.GetSalesPriceAsync(productId, It.IsAny<CancellationToken>())).Returns(salesPrice);
             var query = new CalculateInsuranceQuery { ProductId = productId };
 
             //act
             var insuranceViewModel = await _calculateInsuranceQueryHandler.Handle(query, CancellationToken.None);
 
+            //assert
             Assert.Equal(
                 expected: expectedValue,
                 actual: insuranceViewModel.InsuranceValue
             );
-
         }
     }
 }
