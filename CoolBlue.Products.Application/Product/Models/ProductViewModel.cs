f9d82283f9fb1647727e2d64c43a52fbae5ea8a8
ecesari <ece.sarioglu@ichoosr.com>
2023-11-05 15:24:04 +0100

    removed unnecessary usings

--- CoolBlue.Products.Application/Product/Models/ProductViewModel.cs
+++ CoolBlue.Products.Application/Product/Models/ProductViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CoolBlue.Products.Application.Product.Models
+﻿namespace CoolBlue.Products.Application.Product.Models
 {
     public class ProductViewModel
     {
